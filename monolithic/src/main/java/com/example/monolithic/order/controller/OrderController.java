a125bfa1a323d20641c644996f989cee833fc8f7
leejaehun <ljh0901@mlsoft.com>
2025-08-01 17:17:41 +0900

    feat : lock 추가

--- monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
+++ monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.monolithic.order.controller;
 
 import com.example.monolithic.order.application.OrderService;
+import com.example.monolithic.order.application.RedisLockService;
 import com.example.monolithic.order.application.dto.CreateOrderResult;
 import com.example.monolithic.order.controller.dto.CreateOrderRequest;
 import com.example.monolithic.order.controller.dto.CreateOrderResponse;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+    private final RedisLockService redisLockService;
 
     @PostMapping("/order")
     public CreateOrderResponse createOrder(@RequestBody CreateOrderRequest request) {
@@ -25,6 +27,16 @@ public class OrderController {
 
     @PostMapping("/order/place")
     public void placeOrder(@RequestBody PlaceOrderRequest request) {
-        orderService.placeOrder(request.toPlaceOrderCommand());
+        String key = "order:monolithic:" + request.orderId();
+        boolean acquiredLock = redisLockService.tryLock(key, request.orderId().toString());
+
+        if (!acquiredLock) {
+            throw new RuntimeException("락 획득에 실패하였습니다. 다른 요청이 처리 중일 수 있습니다.");
+        }
+        try {
+            orderService.placeOrder(request.toPlaceOrderCommand());
+        } finally {
+            redisLockService.releaseLock(key);
+        }
     }
 }
